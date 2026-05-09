8e33d31da4321769008fdc727baa6a2d4dd5bffa
gfunk777 <158100201+gfunk777@users.noreply.github.com>
2026-05-09 13:57:37 -0700

    Add SysTick timer header file for STM32L476G

---
+++ Systick_timer.h
@@ -0,0 +1,16 @@
+#ifndef __STM32L476G_SYSTICK_H
+#define __STM32L476G_SYSTICK_H
+
+#include "stm32l476xx.h"
+
+// Modular function to configure SysTick	module
+void SysTick_Init(uint32_t Reload);
+
+// SysTick Exception Handler
+void SysTick_Handler(void);
+
+volatile uint32_t pwm_count;
+volatile uint32_t duty_cycle = 50;
+const MOTOR_PIN = 4;
+
+#endif /* __STM32L476G_SYSTICK_H */
