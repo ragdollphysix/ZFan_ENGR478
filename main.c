dc179b31b788a077a69f44be7dee6dbfd5a30c02
ragdoll <158375016+ragdollphysix@users.noreply.github.com>
2026-05-09 16:03:10 -0700

    0.03

--- main.c
+++ main.c
@@ -9,7 +9,9 @@
 #include "LED.h"
 #include "Systick_timer.h"
 #include "button.h"
-// #include
+#include "ADC.h"
+
+volatile int mode;
 
 
 /* One Motor Driver to Motor
@@ -21,29 +23,31 @@
  *
  * */
 
+
+// MODE MUST START IN MANUAL
+
 int main(void){
 
 	//1. Invoke configure_LED_pin() to initialize PA5 as an output pin, interfacing with the LD2 LED.
 	configure_LED_pin();
-	//2. Turn on the LD2 LED
-	turn_on_LED();
 
-	//3. Initialize PC13 as an input pin
+	//3. Initialize PC2 and PC3 as an input pin
 	configure_Push_Button_pin();
 
 	//4. configure interrupts
 	configure_EXTI();
 
+	// system starts on manual mode and not temperature mode
 	GPIOC->ODR |= 1<<5;
 
-	//3. Invoke SysTick_Init() to set up SysTick periodic exception.
-		//   The default processor clock is 4MHz provided by MSI.
-		//   The SysTick reload value is set to 4,000,000 - 1 to generate periodic exception every 1 s
-		//   For a silent buzzer, enter 0
 
+	mode = 1;
 
  	while(1){
-
+ 		if (mode = 1) {
+ 			NVIC_DisableIRQ(EXTI2_IRQn);
+ 			// enable the temperature interrupt
+ 		}
 
  	}
 }
